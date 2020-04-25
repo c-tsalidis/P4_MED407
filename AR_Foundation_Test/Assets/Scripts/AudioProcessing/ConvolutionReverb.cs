b8ffb7d721642f1783c46d8169797d9dd8baaf4a
c-tsalidis <ctsalidis1999@gmail.com>
2020-04-25 12:06:48 +0200

    Theorized the convolution reverb, now I have to implement it

---
+++ AR_Foundation_Test/Assets/Scripts/AudioProcessing/ConvolutionReverb.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class ConvolutionReverb: MonoBehaviour {
+
+    [SerializeField] private AudioClip input;
+    [SerializeField] private AudioClip impulseResponse;
+    
+    const float MAXLENGTH = 15.0f;
+    const int MAXSAMPLE = 16;
+
+    int globalupdatecount = 0;
+    
+    private void Start() {
+        
+        Debug.Log("Uploading impulse response " + impulseResponse.name);
+        float[] data = new float[impulseResponse.samples];
+        impulseResponse.GetData(data, 0);
+        Convolve(data, data.Length / input.channels, input.channels, input.frequency, input.name);
+    }
+
+    public void Convolve(float[] data, int numsamples, int numchannels, int samplerate, string name) {
+
+        Debug.Log(data.Length);
+        Complex [] complexData = new Complex[data.Length];
+        
+        // to make the data to the power of two --> Check out Zero Padding
+        // Augment the input signal with F-N number of zeros (page 102) --> F is the desired signal length (2^number) --> 2^8 = 256
+        if (!Mathf.IsPowerOfTwo(data.Length)) {
+            ArrayList newData = new ArrayList(data);
+            int F = 256; // desired signal length
+            int N = data.Length;
+            for (int i = 0; i < (F - N); i++) {
+                newData.Add(0);
+            }
+        }
+        
+        // convert data values to complex numbers
+        for(int i = 0; i < data.Length; i++) {
+            complexData[i] = (Complex) data[i];
+        }
+
+        /*
+        Debug.Log(Mathf.IsPowerOfTwo(data.Length));
+        int segments = (int) data.Length / Mathf.Pow(2, 10);
+        for (int i = 0; i < segments; i++) {
+            
+        }
+        */
+        
+        // Apply FFT of input to convert it to frequency domain
+        FourierTransform.FFT(complexData, FourierTransform.Direction.Forward);
+        
+        // apply FFT of impulse response to get the frequency response
+        
+        // apply FIR filter in frequency domain --> multiplication of input in frequency domain with frequency response of filter
+        
+        // IFFT (Inverse Fast Fourier Transform) of the multiplication of input and frequency response
+
+    }
+}
--- AR_Foundation_Test/Assets/Scripts/ConvolutionReverbUploadIR.cs
+++ AR_Foundation_Test/Assets/Scripts/ConvolutionReverbUploadIR.cs
@@ -2,10 +2,9 @@ using UnityEngine;
 using System.Collections;
 using System.Runtime.InteropServices;
 
-public class ConvolutionReverbUploadIR : MonoBehaviour
-{
-    [DllImport("AudioPluginDemo")]
-    private static extern bool ConvolutionReverb_UploadSample(int index, float[] data, int numsamples, int numchannels, int samplerate, [MarshalAs(UnmanagedType.LPStr)] string name);
+public class ConvolutionReverbUploadIR : MonoBehaviour {
+    // [DllImport("AudioPluginDemo")]
+    // private static extern bool ConvolutionReverb_UploadSample(int index, float[] data, int numsamples, int numchannels, int samplerate, [MarshalAs(UnmanagedType.LPStr)] string name);
 
     public AudioClip[] impulse = new AudioClip[0];
     public int index;
@@ -13,30 +12,29 @@ public class ConvolutionReverbUploadIR : MonoBehaviour
     private bool[] uploaded = new bool[64];
     private AudioClip[] currImpulse = new AudioClip[64];
 
-    void Start()
-    {
+    private ConvolutionReverb _convolutionReverb;
+    
+    void Start() {
+        _convolutionReverb = new ConvolutionReverb();
         UploadChangedClips();
     }
 
-    void Update()
-    {
+    void Update() {
         UploadChangedClips();
     }
 
-    void UploadChangedClips()
-    {
+    void UploadChangedClips() {
         int currindex = index;
-        foreach (var s in impulse)
-        {
+        foreach (var s in impulse) {
             if (currImpulse[currindex] != s)
                 uploaded[currindex] = false;
 
-            if (s != null && s.loadState == AudioDataLoadState.Loaded && !uploaded[currindex])
-            {
+            if (s != null && s.loadState == AudioDataLoadState.Loaded && !uploaded[currindex]) {
                 Debug.Log("Uploading impulse response " + s.name + " to slot " + currindex);
                 float[] data = new float[s.samples];
                 s.GetData(data, 0);
-                ConvolutionReverb_UploadSample(currindex, data, data.Length / s.channels, s.channels, s.frequency, s.name);
+                // ConvolutionReverb_UploadSample(currindex, data, data.Length / s.channels, s.channels, s.frequency, s.name);
+                // _convolutionReverb.Convolve(currindex, data, data.Length / s.channels, s.channels, s.frequency, s.name);
                 uploaded[currindex] = true;
                 currImpulse[currindex] = s;
             }
