cf750211447dea16c8ef42adf0a2110334987073
marcus-dyrholm <43436489+marcus-dyrholm@users.noreply.github.com>
2020-04-28 14:23:03 +0200

    #11 Started work on this issue

---
+++ AR_Foundation_Test/Assets/Scripts/AudioMeter.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMeter : MonoBehaviour
+{
+    private const int SAMPLE_SIZE = 1024;
+
+    //average power output of the sound
+    public float rmsValue;
+    public float dbValue;
+    public float pitchValue;
+
+    private AudioSource source;
+    private float[] samples;
+    private float[] spectrum;
+    private float sampleRate;
+
+    private Transform[] visualList;
+    private float[] visualScale;
+    private int amnVisual = 10;
+    
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+        samples = new float[SAMPLE_SIZE];
+        spectrum = new float[SAMPLE_SIZE];
+        sampleRate = AudioSettings.outputSampleRate;
+
+        SpawnLine();
+    }
+    private void SpawnLine()
+    {
+        visualScale = new float[amnVisual];
+        visualList = new Transform[amnVisual];
+
+        for (int i = 0; i < amnVisual; i++)
+        {
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube) as GameObject;
+            visualList[i] = go.transform;
+            visualList[i].position = Vector3.right * i;
+        }
+    }
+    private void Update()
+    {
+        AnalyzeSound();
+    }
+    private void AnalyzeSound()
+    {
+        source.GetOutputData(samples, 0);
+
+        //get rms values
+        int i = 0;
+        float sum = 0;
+        for(; i < SAMPLE_SIZE; i++)
+        {
+            sum += samples[i] * samples[i];
+        }
+        rmsValue = Mathf.Sqrt(sum / SAMPLE_SIZE);
+
+        //Getting the DB value
+        dbValue = 20 * Mathf.Log10(rmsValue / 0.1f);
+
+        //Get sound spectrum
+        source.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        //find pitch
+        float maxV = 0;
+        var maxN = 0;
+
+        for (i = 0; i < SAMPLE_SIZE; i++)
+        { // find max 
+            if (!(spectrum[i] > maxV) || !(spectrum[i] > 0.0f))
+                continue;
+
+            maxV = spectrum[i];
+            maxN = i; // maxN is the index of max
+            
+        }
+        float freqN = maxN; 
+
+        if (maxN > 0 && maxN < SAMPLE_SIZE - 1)
+        { 
+            // interpolate index using neighbours
+            var dL = spectrum[maxN - 1] / spectrum[maxN];
+            var dR = spectrum[maxN + 1] / spectrum[maxN];
+            freqN += 0.5f * (dR * dR - dL * dL);
+        }
+        pitchValue = freqN * (sampleRate / 2) / SAMPLE_SIZE; // convert index to frequency
+    }
+}
+
