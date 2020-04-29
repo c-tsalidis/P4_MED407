206b00f5b37035ba5e37129075c0756a037a513e
marcus-dyrholm <43436489+marcus-dyrholm@users.noreply.github.com>
2020-04-29 13:21:47 +0200

    #11 Made progress

--- AR_Foundation_Test/Assets/Scripts/AudioMeter.cs
+++ AR_Foundation_Test/Assets/Scripts/AudioMeter.cs
@@ -11,6 +11,11 @@ public class AudioMeter : MonoBehaviour
     public float dbValue;
     public float pitchValue;
 
+    public float maxVisualScale = 25;
+    public float visualModifier = 50.0f;
+    public float smoothSpeed = 10.0f;
+    public float keepPercentage = 0.5f;
+
     private AudioSource source;
     private float[] samples;
     private float[] spectrum;
@@ -18,7 +23,7 @@ public class AudioMeter : MonoBehaviour
 
     private Transform[] visualList;
     private float[] visualScale;
-    private int amnVisual = 10;
+    private int amnVisual = 40;
     
     private void Start()
     {
@@ -37,13 +42,47 @@ public class AudioMeter : MonoBehaviour
         for (int i = 0; i < amnVisual; i++)
         {
             GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube) as GameObject;
+            //visualList[i].localScale = Vector3(1, 1, 0.1);
             visualList[i] = go.transform;
             visualList[i].position = Vector3.right * i;
+            
         }
     }
     private void Update()
     {
         AnalyzeSound();
+        UpdateVisual();
+    }
+    private void UpdateVisual() 
+    {
+        int visualIndex = 0;
+        int spectrumIndex = 0;
+        int averageSize = (int)((SAMPLE_SIZE * keepPercentage) / amnVisual);
+
+        while (visualIndex < amnVisual)
+        {
+            int j = 0;
+            float sum = 0;
+
+            while (j < averageSize)
+            {
+                sum += spectrum[spectrumIndex];
+                spectrumIndex++;
+                j++;
+            }
+            float scaleY = sum / averageSize * visualModifier;
+            visualScale[visualIndex] -= Time.deltaTime * smoothSpeed;
+
+            if (visualScale[visualIndex] < scaleY)
+                visualScale[visualIndex] = scaleY;
+
+            if (visualScale[visualIndex] >  maxVisualScale)
+            visualScale[visualIndex] = maxVisualScale;
+            
+
+            visualList[visualIndex].localScale = Vector3.one + Vector3.up * visualScale[visualIndex];
+            visualIndex++;
+        }
     }
     private void AnalyzeSound()
     {
