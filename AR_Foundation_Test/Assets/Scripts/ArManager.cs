cc77986710d2adf6857f7ecf9c9cf26494f02b5b
tbtjems <43847640+tbtjems@users.noreply.github.com>
2020-05-02 16:14:27 +0200

    Fixed issue #12
    Implemented HRTF sound with Resonance Audio on project
    
    Co-Authored-By: balazsivanyi <balazsivanyi@users.noreply.github.com>

--- AR_Foundation_Test/Assets/Scripts/ArManager.cs
+++ AR_Foundation_Test/Assets/Scripts/ArManager.cs
@@ -2,94 +2,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-using Object = System.Object;
-using UnityEngine.UI;
-using Button = UnityEngine.UI.Button;
+
+
 
 [RequireComponent(typeof(ARRaycastManager))]
-public class ArManager : MonoBehaviour {
+public class ArManager : MonoBehaviour
+{
     [SerializeField] private GameObject[] goToPlace;
     private GameObject[] _spawnedGo;
     private ARRaycastManager _arRaycastManager;
     private Vector2 _touchPos;
-    
+
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
-    public Button myButton;
 
-    private void Start() {
+
+    private void Start()
+    {
         _arRaycastManager = gameObject.GetComponent<ARRaycastManager>();
-        _spawnedGo = new GameObject[2]; //initialising two gameobjects
 
-        Button nextSceneButton = myButton.GetComponent<Button>(); //initialising button
-        nextSceneButton.onClick.AddListener(goToNextScene); //adding goToNextScene function when it is pressed
+        _spawnedGo = new GameObject[2];
 
     }
-    
-    private void Update() {
-        if (!CheckTouchPosition(out Vector2 _touchPos)) {
+
+
+
+    private void Update()
+    {
+
+        if (!CheckTouchPosition(out Vector2 _touchPos))
+        {
             return;
         }
 
-        if (_arRaycastManager.Raycast(_touchPos, hits, TrackableType.PlaneWithinPolygon)) {
+        if (_arRaycastManager.Raycast(_touchPos, hits, TrackableType.PlaneWithinPolygon))
+        {
             var hitPose = hits[0].pose;
-            
-            for (int i = 0; i < goToPlace.Length; i++) {
+
+            for (int i = 0; i < goToPlace.Length; i++)
+            {
+                // ReSharper disable once Unity.PerformanceCriticalCodeNullComparison
                 if (_spawnedGo[i] == null)
                 {
                     _spawnedGo[i] = Instantiate(goToPlace[i], hitPose.position, hitPose.rotation);
                 }
                 else
                 {
-                    var x_new = i * 5;
-                    _spawnedGo[i].transform.position = hitPose.position + new Vector3(x_new, 0, 0);
+                    var xNew = i * 5;
+                    _spawnedGo[i].transform.position = hitPose.position + new Vector3(xNew, 0, 0);
                 }
             }
+
         }
-    } 
+    }
 
-    private Vector3 placeSpheres()
+
+
+    private bool CheckTouchPosition(out Vector2 _touchPos)
     {
-        // these positions are just default ones, for testing purposes
-        var xPos = 120; 
-        var yPos = 300;
-        var height = 100;
-
-        var xPosNew = 100;
-        var yPosNew = 100;
-        
-        //TODO: create an array with a length of 10 with vector3 coordinates for the next position of the spheres
-        
-        for (int i = 0; i < _spawnedGo.Length; i++)
+        if (Input.touchCount > 0)
         {
-            if (_spawnedGo[i] == null)
-            {
-                return _spawnedGo[i].transform.position = new Vector3(xPos, yPos, height);
-            }
-            else
-            {
-                return _spawnedGo[i].transform.position = _spawnedGo[i-1].transform.position + new Vector3(xPosNew, yPosNew, 0);
-            }
-        }
-        return default;
-    }
-
-    public void goToNextScene()
-    {
-        placeSpheres(); //calls placeSphere function when proceeding to the next scene
-        
-        //TODO: add signifier gameobjects (10 squares) + functionality to set the next one true (coloured) and the rest false (uncoloured) 
-        
-    }
-
-    private bool CheckTouchPosition(out Vector2 _touchPos) {
-        if (Input.touchCount > 0) {
             _touchPos = Input.GetTouch(0).position;
             return true;
         }
+
+
 
         _touchPos = default;
         return false;
