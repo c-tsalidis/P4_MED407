a8248537d6f8a171e5717890c84b58afd5e00d70
tbtjems <43847640+tbtjems@users.noreply.github.com>
2020-05-02 14:15:03 +0200

    Fixed issue #10
    Missing signifiers, and "sweep" animation

---
+++ AR_Foundation_Test/Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Manages the links between each scene
+/// The method LoadScene(string) loads the scene with the name given as a string parameter
+/// For example, to load the "ShipAR" scene --> Manager.LoadScene("ShipAR");
+/// </summary>
+
+public class MainMenu : MonoBehaviour
+{
+    // loads the scene with the name sceneName
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
