e5b0b4236386503ddab86decb3545e0a2c44f887
marcus-dyrholm <43436489+marcus-dyrholm@users.noreply.github.com>
2020-05-04 18:14:24 +0200

    added audiorec script and edited convolution test

--- AR_Foundation_Test/Assets/Scripts/AudioProcessing/ConvolutionTest.cs
+++ AR_Foundation_Test/Assets/Scripts/AudioProcessing/ConvolutionTest.cs
@@ -5,43 +5,50 @@ using NWaves.Operations;
 using NWaves.Operations.Convolution;
 using NWaves.Signals;
 
+using System.IO;
+using System.Linq;
+
+
 public class ConvolutionTest : MonoBehaviour
 {
-    public AudioSource IR;
-    public AudioSource source;
+    public AudioClip IR;
+    public AudioClip source;
 
     float[] kernel;
     float[] samples;
     float[] output;
 
-    Convolver convolve = new Convolver(512);
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        source = GetComponent<AudioSource>();
-        IR = GetComponent<AudioSource>();
+         //source = GetComponent<AudioClip>();
+        //IR = GetComponent<AudioClip>();
 
-        samples = new float[1024];
-        kernel = new float[1024];
-        output = new float[samples.Length + kernel.Length - 1];
+        samples = new float[source.samples];
+        kernel = new float[IR.samples] ;
+        // output = new float[samples.Length + kernel.Length - 1];
+        AnalyzeSound();
+        SaveOutputSignal(output);
 
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        AnalyzeSound();
+      
     }
 
     void AnalyzeSound()
     {
-        source.GetOutputData(samples, 0);
-        IR.GetOutputData(kernel, 0);
-
+        source.GetData(samples, 0);
+        IR.GetData(kernel, 0);
+        Convolver convolve = new Convolver(Mathf.NextPowerOfTwo(samples.Length + kernel.Length - 1));
+        output = new float[Mathf.NextPowerOfTwo(samples.Length + kernel.Length - 1)];
         convolve.Convolve(samples, kernel, output);
-
+        //Debug.Log(convolve.);
+        /*
         for (int i = 0; i < output.Length; i++)
         {
             System.Diagnostics.Debug.WriteLine(output[i]);
@@ -54,5 +61,13 @@ public class ConvolutionTest : MonoBehaviour
         {
             System.Diagnostics.Debug.WriteLine(kernel[i]);
         }
+        */
+    }
+    private void SaveOutputSignal(float[] outputSignal)
+    {
+        string filename = "outputSignal.txt";
+        // save the output signal to a txt file
+        File.WriteAllLines(Path.Combine(Application.streamingAssetsPath, filename),
+            outputSignal.Select(d => d.ToString()));
     }
 }
