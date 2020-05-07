3b6dc4307ad6d19068daf000a78be400fd00058b
lillajulia <ltath18@student,aau.dk>
2020-05-07 14:52:24 +0200

    #13
    fixed touch, with separate scripts. Doesnt need to be used, since the script in AR Manager is also working just fine, but i didnt waste all these hours not to upload this code.

---
+++ AR_Foundation_Test/Assets/Scripts/Touched.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+
+public class Touched : MonoBehaviour
+{
+    
+    [SerializeField] private TouchedObject[] touchedObjects;
+    
+    [SerializeField]
+    private Color activeColor = Color.red;
+    
+    [SerializeField]
+    private Color inactiveColor = Color.gray;
+    
+    private Vector2 touchPosition = default;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPosition = touch.position;
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    TouchedObject touchedObject = hit.transform.GetComponent<TouchedObject>();
+                    if (touchedObject != null)
+                    {
+                        ActionOnTouch(touchedObject);
+                        Debug.Log("touched");
+                    }
+                }
+            }
+        }
+        
+    }
+
+    void ActionOnTouch(TouchedObject selected)
+    {
+        foreach (TouchedObject current in touchedObjects)
+        {
+            MeshRenderer meshRenderer = current.GetComponent<MeshRenderer>();
+            if (selected != current)
+            {
+                current.IsTouched = false;
+                meshRenderer.material.color = inactiveColor;
+                
+            }
+            else
+            {
+                current.IsTouched = true;
+                meshRenderer.material.color = activeColor;
+            }
+        }
+    }
+}
---
+++ AR_Foundation_Test/Assets/Scripts/TouchedObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchedObject : MonoBehaviour
+{
+    public bool IsTouched { get; set; }
+
+}
+
