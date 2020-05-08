902a8eb01732178049e5cef4a1a86b87e2512f3a
tbtjems <43847640+tbtjems@users.noreply.github.com>
2020-05-08 10:28:45 +0200

    Sound plays on tap of sphere + menu update

--- AR_Foundation_Test/Assets/Scripts/ArManager.cs
+++ AR_Foundation_Test/Assets/Scripts/ArManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-
+using UnityEngine.UI;
 
 
 [RequireComponent(typeof(ARRaycastManager))]
@@ -20,24 +20,41 @@ public class ArManager : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip audioClip;
 
+    public Text roundText;
+    /*
+     * We have N rounds --> N = 10
+     * We would then need to store the corridnates of each sphere 10 times in a vector3 array
+     * Vector3 spheres = new Vector3 [N times amount of spheres] 
+     * Populate the cordinates arrays with random positions
+     * math.random
+     *
+     * int round = 0
+     * Then each round
+     * When the user clicks on "NEXT"... {round++.. updateRound() }
+     *                                 updateround() would  _spawnedGo[i].transform.position = spheres[round + i*N]  .... roundTexttext = round + 1;
+     */
+    
     private void Start()
     {
         _arRaycastManager = gameObject.GetComponent<ARRaycastManager>();
 
         _spawnedGo = new GameObject[2];
-
+        
     }
-
 
 
     private void Update()
     {
         // Debug.Log("Time Since Loaded : " + Time.timeSinceLevelLoad);
-     
-        if (!CheckTouchPosition(out _touchPos)) {
+
+        if (!CheckTouchPosition(out _touchPos))
+        {
             return;
         }
-        if (Input.touchCount > 0) {
+
+        // Play sound on hit
+        if (Input.touchCount > 0)
+        {
             var touch = Input.GetTouch(0);
             if (touch.phase == TouchPhase.Began)
             {
@@ -45,19 +62,16 @@ public class ArManager : MonoBehaviour
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
                 if (Physics.Raycast(ray, out hit, 1000))
                 {
-                    if (hit.transform.CompareTag("TagTry"))
+                    if (hit.transform.CompareTag("Sphere"))
                     {
-                        gameObject.GetComponent<AudioSource>().Play();
+                        hit.transform.GetComponent<AudioSource>().Play();
                     }
                 }
             }
         }
-        if (!CheckTouchPosition(out _touchPos))
-        {
-            return;
-        }
-        
 
+
+        //Spawning of spheres
         if (_arRaycastManager.Raycast(_touchPos, hits, TrackableType.PlaneWithinPolygon))
         {
             var hitPose = hits[0].pose;
@@ -75,16 +89,11 @@ public class ArManager : MonoBehaviour
                     _spawnedGo[i].transform.position = hitPose.position + new Vector3(xNew, 0, 0);
                 }
             }
-
         }
     }
 
-    public void playSound()
-    {
-        audioSource.clip = audioClip;
-        audioSource.Play();
-    }
 
+    // Checks where screen is touched
     private bool CheckTouchPosition(out Vector2 _touchPos)
     {
         if (Input.touchCount > 0)
@@ -92,7 +101,7 @@ public class ArManager : MonoBehaviour
             _touchPos = Input.GetTouch(0).position;
             return true;
         }
-        
+
         _touchPos = default;
         return false;
     }
